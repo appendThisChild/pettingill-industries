842b651f13f939d2e8a85e0d1b938ec4c96e803e
Ryan Pettingill <pettingillryan@gmail.com>
2020-01-03 16:51:00 -0700

    rethinking webdev design

--- client/src/components/WebDevHome.js
+++ client/src/components/WebDevHome.js
@@ -1,30 +1,30 @@
 import React, { useState, useEffect } from "react"
-import Image from "../images/website-development.jpg"
 
 //components
 import Footer from "./Footer.js"
 import Header from "./Header.js"
 import ContactForm from "./ContactForm.js"
+import WebDevSection1 from "./WebDevSection1.js"
+import WebDevSection2 from "./WebDevSection2.js"
 
 const WebDevHome = props => {
     const [ position, setPosition ] = useState("50% 50%")
-    const [ size, setSize ] = useState("100%")
-    const [ transition, setTransition ] = useState(".5s")
+    const [ size, setSize ] = useState("250%")
+    const [ pulsingSize, setPulsingSize ] = useState("webDevBetweenSections1_0")
     const [ timing, setTiming ] = useState("cubic-bezier(.09,1.44,.17,.24)")
-    let myInterval = 0;
+    let myInterval1 = 0;
+    let myInterval2 = 0;
 
     const handleSubmit = message => {
         console.log(message)
     }
 
     const changeBackground = num => {
-        const randomNum1 = Math.floor(Math.random() * 50)
-        setSize(`${25 + randomNum1}%`)
+        const randomNum1 = Math.floor(Math.random() * 100)
+        setSize(`${150 + randomNum1}%`)
         const randomNum2 = Math.floor(Math.random() * 100)
         const randomNum3 = Math.floor(Math.random() * 100)
         setPosition(`${randomNum2}% ${randomNum3}%`)
-        // const randomNum4 = Math.random() * 2 + 1
-        setTransition(`${3}s`)
         const option1 = "cubic-bezier(1,.56,0,1.22)"
         const option2 = "cubic-bezier(.95,.69,.19,1.95)"
         const option3 = "cubic-bezier(.6,-0.95,0,1.17)"
@@ -38,82 +38,78 @@ const WebDevHome = props => {
     }
     const glitchOut = () => {
         let i = 1
-        changeBackground(1)
-        i++
-        myInterval = setInterval(() => {
-            changeBackground(i)
+        changeBackground(i)
+        myInterval1 = setInterval(() => {
             i++
+            changeBackground(i)
             if (i > 3) i = 1;
-        }, 3000);
+        }, 2250);
+    }
+    const pulsing = i => {
+        if ( i === 0){
+            setPulsingSize("webDevBetweenSections1_1")
+        } else {
+            setPulsingSize("webDevBetweenSections1_0")
+        }
+    }
+    const pulse = () => {
+        let i = 0
+        pulsing(i)
+        myInterval2 = setInterval(() => {
+            if ( i === 0){
+                i++
+                pulsing(i)
+                
+            } else {
+                i--
+                pulsing(i)
+                
+            }
+        }, 1250)
     }
     useEffect(() => {
         glitchOut()
+        pulse()
         return () => {
-            clearInterval(myInterval)
+            clearInterval(myInterval1)
+            clearInterval(myInterval2)
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
     return(
         <>
         <Header />
-        <div className="webHome" style={{ backgroundPosition: position, backgroundSize: `auto ${size}`, transition: `all ${transition} ${timing} 0s`}}>
+        
+        <div className="webHome" style={{ backgroundPosition: position, backgroundSize: `${size}`, transition: `all 2.25s ${timing} 0s`}}>
             <div>
-                <h1>Website Development</h1>
-                <div>
-                    <main>
-                        <img src={Image} alt="Working on your Business"/>
-                    </main>
-                    <section>
-                        <h2>Our Mission</h2>
-                        
-                    </section>
-                    <section>
-                        <h2>Services</h2>
-
-                    </section>
-                </div>
-                <ContactForm 
-                    className="webForm"
-                    questions={[
-                    {
-                        question: "Name",
-                        tag: "input",
-                        objTag: "name"
-                    },
-                    {
-                        question: "Email Address",
-                        tag: "input",
-                        objTag: "email"
-                    },
-                    {
-                        question: "Telephone",
-                        tag: "input",
-                        objTag: "number"
-                    },
-                    {
-                        question: "Name of the business:",
-                        tag: "input",
-                        objTag: "businessName"
-                    },
-                    {
-                        question: "Describe the business:",
-                        tag: "textarea",
-                        objTag: "businessAbout"
-                    },
-                    {
-                        question: "What would the website accomplish for the business?",
-                        tag: "textarea",
-                        objTag: "businessWebsite"
-                    },
-                    {
-                        question: "Other:",
-                        tag: "textarea",
-                        objTag: "other"
-                    }
-                    ]}
-                    handleSubmit={handleSubmit} />
+                <h1>Website Design</h1>
+                <h1>Reclaiming Imagination</h1>
             </div>
         </div>
+        <WebDevSection1 />
+        {/* style={{ backgroundSize: `${pulsingSize}`}} */}
+        <div className={`webDevBetweenSections1 ${pulsingSize}`} ></div>
+        <WebDevSection2 />
+        <ContactForm 
+            className="webForm"
+            questions={[
+            {
+                question: "Name",
+                tag: "input",
+                objTag: "name"
+            },
+            {
+                question: "Email Address",
+                tag: "input",
+                objTag: "email"
+            },
+            {
+                question: "Message",
+                tag: "textarea",
+                objTag: "message"
+            }
+            ]}
+            handleSubmit={handleSubmit} />
         <Footer />
         </>
     )
